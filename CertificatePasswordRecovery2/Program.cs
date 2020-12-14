0fe2fb19d0d3f579ac8f74f5f51b8be5d9d42941
Antti K. Koskela <posti@koskila.net>
2020-12-14 21:49:43 +0200

    Modernizing the Windows Forms solution to .NET 5 & adding threading

---
+++ CertificatePasswordRecovery2/Program.cs
@@ -0,0 +1,24 @@
+using CertificatePasswordRecovery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CertificatePasswordRecovery2
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CertPasswordRecoveryForm());
+        }
+    }
+}
